34fce2cd488bcc994df64627a316916cef76b4d0
David Castellanos <davicaste@gmail.com>
2019-08-28 11:59:30 -0500

    Improved hiperText

---
+++ Assets/Scripts/HiperTextManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HiperTextManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject hiperWord;
+    [SerializeField]
+    private GameObject wordRow;
+    [SerializeField]
+    private int rowLength;
+
+    private string text = "Los labs de innovación se han convertido en una pieza clave para apalancar la transformación de las organizaciones. " +
+                          "Les recomiendo este conversatorio en donde, de la mano de tres de los labs más importantes del país, se hablará el por qué, " +
+                          "para qué y cómo de los laboratorios de innovación.";
+    
+    private string[] textArray;
+    private int currentRowLength;
+    private GameObject currentRow;
+
+    void Start()
+    {
+        textArray = text.Split(' ');
+        BuildText();
+    }
+
+    void BuildText()
+    {
+        currentRow = Instantiate(wordRow, transform);
+        foreach(string word in textArray)
+        {
+            currentRowLength += word.Length + 1;
+
+            if(currentRowLength > rowLength)
+            {
+                LayoutRebuilder.ForceRebuildLayoutImmediate((RectTransform)currentRow.transform);
+                currentRow = Instantiate(wordRow, transform);
+                currentRowLength = 0;
+            }
+
+            GameObject newWord = Instantiate(hiperWord, currentRow.transform);
+
+            newWord.GetComponent<HiperWord>().Init(word);
+        }
+
+        LayoutRebuilder.ForceRebuildLayoutImmediate((RectTransform)currentRow.transform);
+
+        //firstRow.GetComponent<HorizontalLayoutGroup>().enabled = true;
+    }
+
+}
---
+++ Assets/Scripts/HiperWord.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HiperWord : MonoBehaviour
+{
+    [SerializeField]
+    private Text sizeText;
+    [SerializeField]
+    private Text visibleText;
+
+    public void Init(string text)
+    {
+        sizeText.text = text;
+        visibleText.text = text;
+    }
+}
